423830e1c9b9e2441185661e4c04ebc35403be8c
Pedro Henrique Martins Belo <pedrohenriqueunicamp2024@gmail.com>
2024-09-17 11:54:02 -0300

    SQL_MySQL

--- Projeto Análise de Dados SQL.sql
+++ Projeto Análise de Dados SQL.sql
@@ -1,3 +1,5 @@
+USE hashtagmovie;
+
 # --- ANÁLISES DE DADOS COM SQL --- #
 
 -- Agrupamentos
@@ -18,24 +20,18 @@ SELECT * FROM filmes;
 
 -- CASE 1. Você deverá começar fazendo uma análise para descobrir o preço médio de aluguel dos filmes.
 
+SELECT AVG(preco_aluguel) as Média FROM filmes;
 
 
--- CASE 2. Agora que você sabe o preço médio para se alugar filmes na hashtagmovie, você deverá ir além na sua análise e descobrir qual é o preço médio para cada gênero de filme.
+/*CASE 2. Agora que você sabe o preço médio para se alugar filmes na hashtagmovie, 
+você deverá ir além na sua análise e descobrir qual é o preço médio para cada gênero de filme.*/
 
-/*
-genero                   | preco_medio
-______________________________________
-Comédia                  | X
-Drama                    | Y
-Ficção e Fantasia        | Z
-Mistério e Suspense      | A
-Arte                     | B
-Animação                 | C
-Ação e Aventura          | D
-*/
-
-
-
+SELECT 
+	genero,
+    ROUND(AVG(preco_aluguel), 2) AS Media_genero,
+    COUNT(*) AS total_filme
+FROM filmes
+GrOUP BY genero;
 
 # =======              PARTE 2:               =======#
 # =======       FILTROS EM AGRUPAMENTOS       =======#
@@ -44,30 +40,30 @@ Ação e Aventura          | D
 
 -- CASE 3: Fazer a mesma análise, mas considerando apenas os filmes com ANO_LANCAMENTO igual a 2011.
 
-
-
-
-
-
+SELECT 
+	genero,
+    ROUND(AVG(preco_aluguel), 2) AS Media_genero,
+    COUNT(*) AS total_filme
+FROM filmes WHERE ano_lancamento = 2011
+GROUP BY genero;
 
 
 # =======                         PARTE 3:                           =======#
 # =======  SUBQUERIES: UTILIZANDO UM SELECT DENTRO DE OUTRO SELECT   =======#
 
--- CASE 4. Você precisará fazer uma análise de desempenho dos alugueis. Para isso, uma análise comum é identificar quais aluguéis tiveram nota acima da média. Você seria capaz de fazer isso?
+/*CASE 4. Você precisará fazer uma análise de desempenho dos alugueis.
+ Para isso, uma análise comum é identificar quais aluguéis tiveram nota acima da média. Você seria capaz de fazer isso?*/
 
-SELECT AVG(nota) FROM alugueis;
+SELECT AVG(nota) FROM alugueis; -- 7.94
 
-
-
+SELECT nota FROM alugueis
+ WHERE nota >= (SELECT AVG(nota) FROM alugueis);
 
 
 # =======           PARTE 4:             =======#
 # =======  Criando Views - CREATE VIEW   =======#
 
-
 -- CREATE/DROP VIEW: Guardando o resultado de uma consulta no nosso banco de dados
-
 
 -- CASE 5. Crie uma view para guardar o resultado do SELECT abaixo.
 
