2ce1cad9877fa2868c5c99ffe1e325189890e526
Pedro Henrique Martins Belo <pedrohenriqueunicamp2024@gmail.com>
2024-09-17 11:38:45 -0300

    SQL/MySQL

---
+++ Projeto Análise de Dados SQL.sql
@@ -0,0 +1,83 @@
+# --- ANÁLISES DE DADOS COM SQL --- #
+
+-- Agrupamentos
+-- Filtragem em agrupamentos
+-- Subqueries
+-- Criação de Views
+
+-- Lembrando das tabelas do banco de dados...
+
+SELECT * FROM alugueis;
+SELECT * FROM atores;
+SELECT * FROM atuacoes;
+SELECT * FROM clientes;
+SELECT * FROM filmes;
+
+# =======        PARTE 1:        =======#
+# =======  CRIANDO AGRUPAMENTOS  =======#
+
+-- CASE 1. Você deverá começar fazendo uma análise para descobrir o preço médio de aluguel dos filmes.
+
+
+
+-- CASE 2. Agora que você sabe o preço médio para se alugar filmes na hashtagmovie, você deverá ir além na sua análise e descobrir qual é o preço médio para cada gênero de filme.
+
+/*
+genero                   | preco_medio
+______________________________________
+Comédia                  | X
+Drama                    | Y
+Ficção e Fantasia        | Z
+Mistério e Suspense      | A
+Arte                     | B
+Animação                 | C
+Ação e Aventura          | D
+*/
+
+
+
+
+# =======              PARTE 2:               =======#
+# =======       FILTROS EM AGRUPAMENTOS       =======#
+
+-- Você deve alterar a consulta DO CASE 2 e considerar o seguinte cenário:
+
+-- CASE 3: Fazer a mesma análise, mas considerando apenas os filmes com ANO_LANCAMENTO igual a 2011.
+
+
+
+
+
+
+
+
+# =======                         PARTE 3:                           =======#
+# =======  SUBQUERIES: UTILIZANDO UM SELECT DENTRO DE OUTRO SELECT   =======#
+
+-- CASE 4. Você precisará fazer uma análise de desempenho dos alugueis. Para isso, uma análise comum é identificar quais aluguéis tiveram nota acima da média. Você seria capaz de fazer isso?
+
+SELECT AVG(nota) FROM alugueis;
+
+
+
+
+
+# =======           PARTE 4:             =======#
+# =======  Criando Views - CREATE VIEW   =======#
+
+
+-- CREATE/DROP VIEW: Guardando o resultado de uma consulta no nosso banco de dados
+
+
+-- CASE 5. Crie uma view para guardar o resultado do SELECT abaixo.
+
+CREATE VIEW resultado AS
+SELECT
+	genero,
+	ROUND(AVG(preco_aluguel), 2) AS media_preco,
+    COUNT(*) AS qtd_filmes
+FROM filmes
+GROUP BY genero;
+
+SELECT * FROM resultado;
+
